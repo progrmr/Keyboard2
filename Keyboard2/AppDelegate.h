7113a38fa3c7d84c2dd61d3bcdf8b534eb911db2
Gary Morris <garym@garym-mac-mini.home>
2014-09-29 21:14:38 -0700

    Initial Commit

---
+++ Keyboard2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Keyboard2
+//
+//  Created by Gary Morris on 9/29/14.
+//  Copyright (c) 2014 Gary Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Keyboard2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Keyboard2
+//
+//  Created by Gary Morris on 9/29/14.
+//  Copyright (c) 2014 Gary Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
